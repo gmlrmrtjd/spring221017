024f47aa0698c3593af935bfac742242cab7f8bf
gmlrmrtjd <gmlrmrtjd@naver.com>
2022-11-10 12:19:34 +0900

    aws s3에  파일 올리기(아직 작성하면 오류남)

--- src/main/java/org/zerock/service/board/BoardService.java
+++ src/main/java/org/zerock/service/board/BoardService.java
@@ -3,7 +3,10 @@ package org.zerock.service.board;
 import java.io.File;
 import java.util.List;
 
+import javax.management.RuntimeErrorException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,6 +14,12 @@ import org.zerock.domain.board.BoardDto;
 import org.zerock.domain.board.PageInfo;
 import org.zerock.mapper.board.BoardMapper;
 import org.zerock.mapper.board.ReplyMapper;
+
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
+import software.amazon.awssdk.services.s3.model.PutObjectAclRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Transactional
 @Service
@@ -21,6 +30,12 @@ public class BoardService {
 
 	@Autowired
 	private ReplyMapper replyMapper;
+	
+	@Autowired
+	private S3Client s3Client;
+	
+	@Value("aws.s3.bucket")
+	private String bucketName;
 
 	public int register(BoardDto board, MultipartFile[] files) {
 		// db에 게시물 정보 저장
@@ -32,17 +47,39 @@ public class BoardService {
 				boardMapper.insertFile(board.getId(), file.getOriginalFilename());
 
 				// 파일 저장
-				// board id 이름의 새폴더 만들기
-				File folder = new File(
-						"/Users/sunggyu-lim/Desktop/kukbi/study/upload/prj1/board/" + board.getId());
-				folder.mkdirs();
-
-				File dest = new File(folder, file.getOriginalFilename());
-
+//				// board id 이름의 새폴더 만들기
+//				File folder = new File(
+//						"/Users/sunggyu-lim/Desktop/kukbi/study/upload/prj1/board/" + board.getId());
+//				folder.mkdirs();
+//
+//				File dest = new File(folder, file.getOriginalFilename());
+//
+//				try {
+//					file.transferTo(dest);
+//				} catch (Exception e) {
+//					// @Transactional은 RuntimeException에서만 rollback 됨
+//					e.printStackTrace();
+//					throw new RuntimeException(e);
+//				}
+				
 				try {
-					file.transferTo(dest);
+					// s3에 파일 저장
+					// 키 생성
+					String key = "prj/board/" + board.getId() + "/" + file.getOriginalFilename();
+					
+					// putObjectRequest
+					PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+							.bucket(bucketName)
+							.key(key)
+							.acl(ObjectCannedACL.PUBLIC_READ)
+							.build();
+					
+					// requestBody
+					RequestBody requestBody = RequestBody.fromInputStream(file.getInputStream(), file.getSize());
+					
+					// object(파일) 올리기 
+					s3Client.putObject(putObjectRequest, requestBody);
 				} catch (Exception e) {
-					// @Transactional은 RuntimeException에서만 rollback 됨
 					e.printStackTrace();
 					throw new RuntimeException(e);
 				}
